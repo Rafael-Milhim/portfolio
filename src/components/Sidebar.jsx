28aed8825b618f7c09c317dafe69c5f64ec8209b
Rafael-Milhim <rafael.milhim@hotmail.com>
2023-09-15 10:23:42 -0300

    Componentes Redes Sociais

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,14 @@
-import Avatar from '../img/foto-perfil.png'
 import '../styles/components/sidebar.sass'
+import SocialNetworks from './SocialNetworks'
+
+import Avatar from '../img/foto-perfil.png'
 
 const Sidebar = () => {
     return (
         <aside id="sidebar">
             <img src={Avatar} alt="Rafael Milhim" />
             <p className="title">Desenvolvedor</p>
-            <p>redes sociais</p>
+            <SocialNetworks />
             <p>informações de contato</p>
             <a href="" className="btn">Download Curriculo</a>
         </aside>
